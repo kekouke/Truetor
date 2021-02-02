9d9e4d4fecdc166c80e2f2687f887ee44f78a48c
Mikhail Kozitskii <kekouke@yandex.ru>
2021-02-02 13:38:13 +1000

    Start working on profile image

--- truetor/apps/userprofile/models.py
+++ truetor/apps/userprofile/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='uploads/', blank=True, null=True)
     follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
 
 User.userprofile = property(lambda u:UserProfile.objects.get_or_create(user=u)[0])

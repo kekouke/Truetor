a3e800ac4a7f797523ad8a1023660e514da5a8d3
Mikhail Kozitskii <38981667+kekouke@users.noreply.github.com>
2021-02-03 03:37:23 +1000

    Update settings.py

--- truetor/settings.py
+++ truetor/settings.py
@@ -143,5 +143,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 import dj_database_url
-prod_db  =  dj_database_url.config()
+prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)

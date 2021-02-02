87a712ef77d48d98da8210295662a32ee7aae444
Mikhail Kozitskii <38981667+kekouke@users.noreply.github.com>
2021-02-03 03:21:16 +1000

    Update settings.py

--- truetor/settings.py
+++ truetor/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$#pga%r24j-)^#df(uy=(j18fbumfc3o%-0a7wrf&q8l3zj$@r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
